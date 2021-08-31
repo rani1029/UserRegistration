893c4276d0379d401bcea918913be92c2678c107
rani1029 <ranig1029@gmail.com>
2021-08-31 18:13:52 +0530

    firstname validation

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            UserEntryValidation.FirstName();
+        }
+    }
+}
---
+++ UserRegistration/UserEntryValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UserEntryValidation
+    {
+        public static void FirstName()
+        {
+            string RegexFirstName = "^[A - Z]{ 1}[a-z]{ 2,}$";
+
+            Console.WriteLine("enter valid first name");
+
+            string Firstname = Console.ReadLine();
+
+            bool validation = Regex.IsMatch(Firstname, RegexFirstName);
+            Console.WriteLine("firstname is valid " + validation);
+        }
+    }
+}
